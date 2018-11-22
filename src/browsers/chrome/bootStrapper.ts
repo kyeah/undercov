f0a918ca2c4f95e6644f81575cd407d3bdd18087
Kevin Yeh <kevinyeah@utexas.edu>
2018-11-21 22:31:03 -0500

    Remove semicolons

--- src/browsers/chrome/bootStrapper.ts
+++ src/browsers/chrome/bootStrapper.ts
@@ -1,77 +1,77 @@
 /// <reference path="../../../typings/tsd.d.ts"/>
-import GithubWindow from '../../githubWindow';
-import { OverlayWindow } from '../../overlayWindow';
-import { ChromeStorage } from './chromeStorage';
-import { IStorageObject } from '../../storageObject';
+import GithubWindow from '../../githubWindow'
+import { OverlayWindow } from '../../overlayWindow'
+import { ChromeStorage } from './chromeStorage'
+import { IStorageObject } from '../../storageObject'
 
 /**
  * Initialize overlay window instance when document is loaded.
  */
 class BootStrapper {
-  private static preferences: IStorageObject = null;
-  private overlay: OverlayWindow = null;
-  private storage: ChromeStorage = new ChromeStorage();
-  private url: string = '';
+  private static preferences: IStorageObject = null
+  private overlay: OverlayWindow = null
+  private storage: ChromeStorage = new ChromeStorage()
+  private url: string = ''
 
   constructor(private context: HTMLDocument) {
-    this.initialize();
+    this.initialize()
   }
 
     private createOverlay(preferences: IStorageObject): OverlayWindow {
       if (this.overlay) {
-            return this.overlay;
+            return this.overlay
         }
-    let doc = document.getElementById('chrome-install-plugin');
+    let doc = document.getElementById('chrome-install-plugin')
     if (doc) {
-      doc.style.display = 'none';
+      doc.style.display = 'none'
     }
-    this.url = preferences.debug_url || document.URL;
+    this.url = preferences.debug_url || document.URL
     if (!(this.url.indexOf('https://github.com') < 0)) {
-      return new GithubWindow(preferences, this.storage);
+      return new GithubWindow(preferences, this.storage)
     }
-    return null;
+    return null
   }
 
   private initialize(): void {
     if (BootStrapper.preferences !== null) {
-      this.setupOverlay();
-      return;
+      this.setupOverlay()
+      return
     }
 
     this.storage.loadOption((preferences: IStorageObject) => {
-      BootStrapper.preferences = preferences;
-      this.setupOverlay();
-    });
+      BootStrapper.preferences = preferences
+      this.setupOverlay()
+    })
   }
 
   private setupOverlay(): void {
     if (!BootStrapper.preferences.overlayEnabled) {
-      return;
+      return
     }
 
-    this.overlay = this.createOverlay(BootStrapper.preferences);
+    this.overlay = this.createOverlay(BootStrapper.preferences)
 
     window.addEventListener('message', (event: MessageEvent) => {
       if (event.source === window &&
           event.data.type) {
           if (event.data.type === 'coveralls' || event.data.type === 'url_change') {
-              this.overlay.log('::pjax-event-received');
-              return this.overlay.initialize();
+              this.overlay.log('::pjax-event-received')
+              return this.overlay.initialize()
           }
       }
-      return null;
-    });
+      return null
+    })
 
     setInterval(() => {
         if (document.URL != this.url) {
-            this.url = document.URL;
-            this.overlay = this.createOverlay(BootStrapper.preferences);
-            this.overlay.log('::hashchange-event-received');
-            return this.overlay.initialize();
+            this.url = document.URL
+            this.overlay = this.createOverlay(BootStrapper.preferences)
+            this.overlay.log('::hashchange-event-received')
+            return this.overlay.initialize()
         }
-    }, 1000);
+    }, 1000)
 
-      this.injectListener();
+      this.injectListener()
   }
 
   /**
@@ -82,19 +82,19 @@ class BootStrapper {
       if ((<any>window).jQuery !== undefined &&
       ($('meta[property="og:site_name"]').attr('content') === 'GitHub')) {
         $(document).on('pjax:success', function () {
-          window.postMessage({ type: 'coveralls' }, '*');
-        });
+          window.postMessage({ type: 'coveralls' }, '*')
+        })
       }
-    } + ')();';
-    let script = document.createElement('script');
-    let element = document.head || document.documentElement;
+    } + ')();'
+    let script = document.createElement('script')
+    let element = document.head || document.documentElement
 
-    script.textContent = listener;
-    element.appendChild(script);
-    script.parentNode.removeChild(script);
+    script.textContent = listener
+    element.appendChild(script)
+    script.parentNode.removeChild(script)
   }
 }
 
 $(() => {
-  new BootStrapper(this);
-});
+  new BootStrapper(this)
+})
--- src/storageObject.ts
+++ src/storageObject.ts
@@ -1,41 +1,41 @@
 export interface IStorageObject {
-  overlayEnabled: boolean;
-  debugEnabled: boolean;
-  debug_url: any;
-  urls: Array<any>;
-  setupUrl(url: string): void;
+  overlayEnabled: boolean
+  debugEnabled: boolean
+  debug_url: any
+  urls: Array<any>
+  setupUrl(url: string): void
 }
 
 export class StorageObject implements IStorageObject {
-  private _urls: Array<string> = [];
+  private _urls: Array<string> = []
 
   get overlayEnabled(): boolean {
-    return this._overlayEnabled;
+    return this._overlayEnabled
   }
   set overlayEnabled(value: boolean) {
-    this._overlayEnabled = value;
+    this._overlayEnabled = value
   }
 
   get debugEnabled(): boolean {
-    return this._debugEnabled;
+    return this._debugEnabled
   }
 
   set debugEnabled(value: boolean) {
-    this._debugEnabled = value;
+    this._debugEnabled = value
   }
 
   get debug_url(): any {
-    return false;
+    return false
   }
 
   get urls(): Array<string> {
-    return this._urls;
+    return this._urls
   }
 
   setupUrl(url: string): void {
-    let href = (this.debug_url || document.URL).split('/');
+    let href = (this.debug_url || document.URL).split('/')
     if (href.length > 2 && href[2].indexOf('github.com') > 0) {
-      this._urls.push('https://coveralls.io');
+      this._urls.push('https://coveralls.io')
     }
   }
 
